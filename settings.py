2b5f1a047871245c778fad24beecd65f89a3b084
Hanyu Xu <hanyu.xu@aol.com>
2026-07-30 19:18:16 +0200

    add configuration management for Google Maps API key

--- settings.py
+++ settings.py
@@ -1,3 +1,5 @@
 from zoneinfo import ZoneInfo
+from pathlib import Path
 
 TIMEZONE = ZoneInfo("Europe/Zurich")
+CONFIG_FILE = Path(__file__).parent.parent / "config" / "config.ini"
---
+++ utils/config_manager.py
@@ -0,0 +1,8 @@
+import configparser
+from settings import CONFIG_FILE
+
+
+def get_google_maps_api_key():
+    config = configparser.ConfigParser()
+    config.read(CONFIG_FILE)
+    return config.get("google_maps", "api_key")
